62f52bffa300cd33bb6293e4938a34d1d34fa73d
Rob <106797059+rpribau@users.noreply.github.com>
2023-05-31 09:17:22 -0600

    Front page almost done.

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,3 +1,6 @@
+import {FaGoogle} from 'react-icons/fa'
+
+
 const NavBar = () => {
     return (
         <div className="navbar bg-base-100">
@@ -40,7 +43,10 @@ const NavBar = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Get started</a>
+  <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow flex items-center">
+      <FaGoogle className="mr-2" />
+      <p className="text-xl">Log in with Google</p>
+    </button>
   </div>
 </div>
     )
